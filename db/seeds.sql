fafd8d9ae5c68063c78689b5004ec71208c8b5a8
PupMafia <joshsan111@gmail.com>
2018-05-09 23:29:09 -0400

    burger app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+insert into burgers (burger_name, devoured) values ("Angus Burger", false);
+insert into burgers (burger_name, devoured) values ("Chicken Burger", false);
+insert into burgers (burger_name, devoured) values ("Veggie Burger", false);
+insert into burgers (burger_name, devoured) values ("Quinoa Burger", true);
+insert into burgers (burger_name, devoured) values ("Kale Burger", true);
